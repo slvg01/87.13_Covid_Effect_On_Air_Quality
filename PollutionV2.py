1b6c1273dec0c834084b70656a33f166feaea0bc
slvg01 <sylvain.legay@gmail.com>
2025-02-23 18:51:31 +0100

    debug_4 2302

--- PollutionV2.py
+++ PollutionV2.py
@@ -60,26 +60,27 @@ df_locations_raw.to_csv('pollution_data/1_locations_raw.csv', index=True)
 #Unpack countries and coordinates
 df_country = df_locations_raw['country'].apply(pd.Series)
 df_coordinates = df_locations_raw['coordinates'].apply(pd.Series)
-df_DatetimeFirst = df_locations_raw['DatetimeFirst'].apply(pd.Series)
-df_Datetimelast = df_locations_raw['DatetimeLastt'].apply(pd.Series)
+df_DatetimeFirst = df_locations_raw['datetimeFirst'].apply(pd.Series)
+df_Datetimelast = df_locations_raw['datetimeLast'].apply(pd.Series)
 
 df_country.columns= ['country_id', 'country_code', 'country_name']
 df_coordinates.columns= ['latitude', 'longitude']
+df_DatetimeFirst.columns= ['datetimeFirst.utc', 'datetimeFirst.local']
+df_Datetimelast.columns= ['datetimeLast.utc', 'datetimeLast.local']
+df_locations_unpacked=pd.concat([df_locations_raw,df_country, df_coordinates, df_DatetimeFirst, df_Datetimelast],axis=1)
 
-df_locations_unpacked=pd.concat([df_locations_raw,df_country, df_coordinates],axis=1)
-df_locations_unpacked.to_csv('pollution_data/2_locations_unpacked.csv', index=True)
 
-
-
-# keep only useful columns
-df_locations_unpacked.drop(['owner', 'provider', 'isMobile', 'isMonitor', 'instruments', 'licenses', 'bounds', 'distance', 'country', 'coordinates'], axis=1, inplace=True,errors='ignore')
+# Pack & clean 
+df_locations_unpacked.drop(['owner', 'provider', 'isMobile', 'isMonitor', 'instruments', 'licenses', 'bounds', 'distance', 'country', 'coordinates', 'datetimeFirst.utc', 'datetimeLast.utc'], axis=1, inplace=True,errors='ignore')
 df_locations_clean = df_locations_unpacked
-df_locations_clean.to_csv('pollution_data/3_locations_clean.csv', index=True)
+df_locations_clean.to_csv('pollution_data/2_locations_clean.csv', index=True)
 
 
 
 # Unpack  the 'sensors' list of sensors : vertically
 df_exploded = df_locations_clean.explode("sensors")
+df_exploded.to_csv('pollution_data/3_exploded.csv', index=True)
+
 
 
 
@@ -88,10 +89,9 @@ sensors_normalized = pd.json_normalize(df_exploded['sensors'])
 
 
 
-# Concatenate expanded sensor data exploded DataFrame
+# Concatenate all dataframe
 df_exploded = pd.concat([df_exploded.reset_index(drop=True), sensors_normalized], axis=1)
-
-
+df_exploded.to_csv('pollution_data/4_exploded.csv', index=True)
 
 # Clean the DataFrame 
 df_exploded["sensor_id"] = df_exploded["id"]
@@ -99,7 +99,11 @@ df_exploded["sensor_name"] = df_exploded["parameter.name"]
 df_exploded["parameter_id"] = df_exploded["parameter.id"]
 df_exploded["units"] = df_exploded["parameter.units"]
 df_exploded["parameter_name"] = df_exploded["parameter.displayName"]
-df_exploded.drop(['sensors', 'id', 'name', 'parameter.id' ,'parameter.name', 'parameter.units', 'parameter.displayName'], axis=1, inplace=True)
+df_exploded["datetimeFirst_local"] = df_exploded["datetimeFirst.local"]
+df_exploded["datetimeLast_local"] = df_exploded["datetimeLast.local"]
+df_exploded.drop(['sensors', 'id', 'name', 'parameter.id' ,'parameter.name', 'parameter.units', 'parameter.displayName', 'datetimeFirst', 'datetimeLast'], axis=1, inplace=True)
+
+
 #df_exploded.reset_index(drop=True, inplace=True)
 df_location_final = df_exploded
 df_location_final.to_csv('pollution_data/4_location_final.csv', index=True)
@@ -109,22 +113,24 @@ print(f'location_final lenght : {len(df_location_final)}')
 # restric to france geography
 df_location_final_france = df_location_final[df_location_final['country_code']=='FR']
 df_location_final_france.to_csv('pollution_data/5_location_final_france.csv', index=True)
-print(f'location_france length :  {len(df_location_final_france)}')
+print(f'location_final__france length :  {len(df_location_final_france)}')
 
 
 
 # generate URLs list
-urls_list = [f"https://api.openaq.org/v3/sensors/{sensor_id}/days/monthly" for sensor_id in df_exploded['sensor_id']]
-print(f'length url list: {len(urls_list)}')
+urls_list = [f"https://api.openaq.org/v3/sensors/{sensor_id}/days/monthly" for sensor_id in df_location_final_france['sensor_id']]
+print(f'length url list : {len(urls_list)}')
 
 
-'''
+
 # Extract monthly measurements sensor data from URLs
 data_df = pd.DataFrame()
-
+counter = 0
 for url in urls_list: 
     sensor_id = int(url.split("/")[-3])
     sensors_data = extract_data(url)
+    counter += 1
+    
     time.sleep(1)
     
     data = []
@@ -139,7 +145,7 @@ for url in urls_list:
     
     # Append the current URL's data to the main DataFrame
     data_df = pd.concat([data_df, data_df_url], ignore_index=True)
-    
+    print(f"URL {counter} >> data extracted ")
 
 
 # Unpack the 'summary' column
@@ -172,7 +178,5 @@ coverage_df = pd.json_normalize(data_df['coverage'])
 final_data_df = pd.concat([data_df, coverage_df], axis=1).drop('coverage', axis=1)
 final_data_df.drop(['flagInfo', 'period', 'parameter', 'coordinates', 'expectedInterval', 'observedInterval', 'datetimeFrom.utc', 'datetimeTo.utc','q02', 'q25', 'q75', 'q98'], axis=1, inplace=True, errors='ignore')
 print(len(data_df))
-#final_data_df.to_csv('pollution_data/6_sensor_data_final.csv', index=True)
-
-'''
+final_data_df.to_csv('pollution_data/6_sensor_data_final.csv', index=True)
 
