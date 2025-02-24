42d55cc85be7e4782a74d7b4c2b5a171e0399a45
slvg01 <sylvain.legay@gmail.com>
2025-02-24 16:14:33 +0100

    pbi ingestion clean file

--- pbi_Ingestion_code.py
+++ pbi_Ingestion_code.py
@@ -1,16 +1,12 @@
 import requests
 import pandas as pd
 import time
-import logging
 
 
 
 
 
 # ======= LOGGING CONFIG GLOBAL PARAMTERS DEFINITION  =======
-
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 MAX_RETRIES = 2  # Max attemt in case of extraction failure
@@ -20,7 +16,7 @@ HEADERS = {"X-API-Key": API_KEY, "Accept-Charset": "utf-8"}
 
 
 
-
+    
 
 # ======= FUNCTIONS  DEFINITION  =======
 
@@ -62,16 +58,13 @@ def extract_with_retry(url, max_retries=MAX_RETRIES, delay=RETRY_DELAY):
             error_message = str(e)
 
             if "Too Many Requests" in error_message or "429" in error_message:
-                logging.warning(f"⚠️ Too Many Requests reached on url : {url}")
                 time.sleep(delay)  # Pause entre les tentatives
                 retries += 1
-                logging.warning(f"⚠️ Retry {retries}/{max_retries} after 429 error on {url}")
+                
                 
             else:
-                logging.error(f"❌ Fatal error on {url}: {error_message}")
                 return None #  others errors are fatal > out 
 
-    logging.error(f" ❌ Échec définitif après {MAX_RETRIES+1} tentatives: {url}")
     return None  # max retries reached without success > out 
 
 
@@ -87,7 +80,7 @@ locations_raw = extract_with_retry(url)
 
 df_locations_raw = pd.DataFrame(locations_raw)
 df_locations_raw.rename(columns={'id': 'location_id', 'name': 'location_name'}, inplace=True)
-#df_locations_raw.to_csv('pollution_data/1_locations_raw.csv', index=True)
+
 
 
 # Unpack countries and coordinates and time inside the Dataframe
@@ -106,12 +99,12 @@ df_locations_unpacked=pd.concat([df_locations_raw,df_country, df_coordinates, df
 # Clean the Dataframe
 df_locations_unpacked.drop(['owner', 'provider', 'isMobile', 'isMonitor', 'instruments', 'licenses', 'bounds', 'distance', 'country', 'coordinates', 'datetimeFirst.utc', 'datetimeLast.utc'], axis=1, inplace=True,errors='ignore')
 df_locations_clean = df_locations_unpacked
-#df_locations_clean.to_csv('pollution_data/2_locations_clean.csv', index=True)
+
 
 
 # Unpack  the 'sensors' list of sensors vertically
 df_exploded = df_locations_clean.explode("sensors")
-#df_exploded.to_csv('pollution_data/3_exploded.csv', index=True)
+
 
 
 # Expand the nested dictionary structure of the 'sensors' column horizontally
@@ -120,7 +113,7 @@ sensors_normalized = pd.json_normalize(df_exploded['sensors'])
 
 # Aggregate both Dataframes horizontally
 df_exploded = pd.concat([df_exploded.reset_index(drop=True), sensors_normalized], axis=1)
-#df_exploded.to_csv('pollution_data/4_exploded.csv', index=True)
+
 
 
 # Clean the DataFrame 
@@ -133,8 +126,7 @@ df_exploded["datetimeFirst_local"] = df_exploded["datetimeFirst.local"]
 df_exploded["datetimeLast_local"] = df_exploded["datetimeLast.local"]
 df_exploded.drop(['sensors', 'id', 'name', 'parameter.id' ,'parameter.name', 'parameter.units', 'parameter.displayName', 'datetimeFirst', 'datetimeLast'], axis=1, inplace=True)
 df_location_final = df_exploded
-#df_location_final.to_csv('pollution_data/5_location_final.csv', index=True)
-#print(f'location_final lenght : {len(df_location_final)}')
+
 
 
 
@@ -143,14 +135,13 @@ df_location_final = df_exploded
 
 # restric to luxembourg geography
 df_location_final_lux = df_location_final[df_location_final['country_code']=='LU']
-df_location_final_lux.to_csv('pollution_data/6_location_final_lux.csv', index=True)
-print(f'location_final_lux length :  {len(df_location_final_lux)}')
+
 
 
 
 # Generate URLs list
 urls_list = [f"https://api.openaq.org/v3/sensors/{sensor_id}/days/monthly" for sensor_id in df_location_final_lux['sensor_id']]
-print(f'length url list : {len(urls_list)}')
+
 
 
 
@@ -165,7 +156,6 @@ for counter, url in enumerate(urls_list, start=1):
     sensors_data = extract_with_retry(url)
 
     if sensors_data is None:
-        logging.error(f"❌ Abandonning URL: {url}")
         continue  
 
   
@@ -175,7 +165,7 @@ for counter, url in enumerate(urls_list, start=1):
 
     # Dataframe conversion and appending
     data_df = pd.concat([data_df, pd.DataFrame(sensors_data)], ignore_index=True)
-    logging.info(f"✅ URL {counter}, for sensor id {sensor_id} >> Data extracted sucessfully :)")
+
 
 
     # Impose time delay to avoud API limit (60 call/s and 2K calls/h)
@@ -190,8 +180,7 @@ for counter, url in enumerate(urls_list, start=1):
 if 'summary' in data_df.columns:
     summary_df = pd.json_normalize(data_df['summary'])
     data_df = pd.concat([data_df, summary_df], axis=1).drop(columns=['summary'], errors='ignore')
-else:
-    logging.warning("the column 'summary' is missing in the DataFrame.")
+
 
 # Unpack Period column (time data) 
 if 'period' in data_df.columns:
@@ -204,25 +193,15 @@ if 'period' in data_df.columns:
     data_df['day'] = data_df['period'].apply(
         lambda x: pd.to_datetime(x['datetimeFrom']['local']).strftime('%d') if isinstance(x, dict) and 'datetimeFrom' in x else None
     )
-else:
-    logging.warning("the column 'period' is missing in the DataFrame.")
+
 
 # Unpack coverage column
 if 'coverage' in data_df.columns:
     coverage_df = pd.json_normalize(data_df['coverage'])
     data_df = pd.concat([data_df, coverage_df], axis=1).drop(columns=['coverage'], errors='ignore')
-else:
-    logging.warning("the column 'coverage' is missing in the DataFrame.")
+
 
 # Kill useless columns
 cols_to_drop = ['flagInfo', 'period', 'parameter', 'coordinates', 'expectedInterval', 'observedInterval',
                 'datetimeFrom.utc', 'datetimeTo.utc', 'q02', 'q25', 'q75', 'q98']
 data_df.drop(columns=cols_to_drop, errors='ignore', inplace=True)
-
-
-
-
-# ======= FINAL EXPORT =======
-
-#data_df.to_csv("pollution_data/6_sensor_data_final.csv", index=True)
-logging.info(f"✅ Extraction completed with {len(data_df)} lines recorded for {data_df['sensor_id'].nunique() if 'sensor_id' in data_df.columns else 'N/A'} sensors.")
